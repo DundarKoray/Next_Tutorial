6944188dc19e58f057397b3d3bf66c55ef240643
Koray Dundar <koraydundar_40@hotmail.com>
2023-01-30 15:33:47 +0200

    fetched data from API

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,27 @@
-const Ninjas = () => {
+import styles from '../../styles/Ninjas.module.css';
+
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+  
+    return {
+        //it returns an object which called ninjas and it is equal to data which is defined above
+        props: { ninjas: data }
+    }
+}
+
+const Ninjas = ({ ninjas }) => {
+    console.log(ninjas);
     return ( 
         <>
-            <h1>Hello from ninjas</h1>
+            <h1>Hi all ninjas</h1>
+            {ninjas.map((ninja)=>{
+                return (
+                    <div key={ninja.id}>
+                        <a className={styles.ninja}><h3>{ninja.name}</h3></a>
+                    </div>
+                )
+            })}
         </>
     );
 }
