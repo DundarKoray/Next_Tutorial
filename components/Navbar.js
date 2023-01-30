e4c62eecdadfd34cc00deb0370343d4a489d906c
Koray Dundar <koraydundar_40@hotmail.com>
2023-01-30 13:47:00 +0200

    Added logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
     return ( 
         <nav>
             <div className="logo">
-                <h1>Ninjalist</h1>
+                <Image src='/logo.png' width={128} height={77} alt="company logo" />
             </div>
             <Link href="/" legacyBehavior><a>Home</a></Link>
             <Link href="/about" legacyBehavior><a>About</a></Link>
