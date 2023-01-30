9075c7ef1f4d2968fd4c8685064cc16199326bfb
Koray Dundar <koraydundar_40@hotmail.com>
2023-01-30 13:50:33 +0200

    Changed the structure a bit

--- pages/about.js
+++ pages/about.js
@@ -7,8 +7,10 @@ const About = () => {
                 <title>Ninja list | About</title>
                 <meta name="keywords" content="ninjas"/>
             </Head>
-            <h1>About Page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+            <div>
+                <h1>About Page</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+            </div>
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,12 @@ export default function Home() {
         <title>Ninja list | Home</title>
         <meta name="keywords" content="ninjas"/>
       </Head>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-      <Link href="/ninjas" legacyBehavior><a className={styles.btn}>See all Ninjas</a></Link>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+        <Link href="/ninjas" legacyBehavior><a className={styles.btn}>See all Ninjas</a></Link>
+      </div>
     </>
   )
 }
