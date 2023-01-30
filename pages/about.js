5e6aef858f88ec390c157deaa6f7798e55ff03d4
Koray Dundar <koraydundar_40@hotmail.com>
2023-01-30 13:47:14 +0200

    Added Head

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,15 @@
+import Head from 'next/head';
+
 const About = () => {
     return (
-        <div>
+        <>
+            <Head>
+                <title>Ninja list | Home</title>
+                <meta name="keywords" content="ninjas"/>
+            </Head>
             <h1>About Page</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-        </div>
+        </>
     )
 }
  
